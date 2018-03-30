d023a0cb2b743740a52addccef68f4ae3d6297af
Dustin Baugh <dsbaugh525@gmail.com>
2018-03-30 05:00:55 -0500

    Added some additional comments to the search component

--- front-end/src/app/search/search.component.ts
+++ front-end/src/app/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MongoProductsService} from '../services/mongo-products.service';
 import {DatePipe} from '@angular/common';
 
@@ -6,7 +6,7 @@ import {DatePipe} from '@angular/common';
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css'],
-  providers: [ DatePipe ]
+  providers: [DatePipe]
 })
 export class SearchComponent implements OnInit {
 
@@ -43,11 +43,14 @@ export class SearchComponent implements OnInit {
     }
   };
 
-  defaultRequest = { ...this.productRequest};
+  defaultRequest = {...this.productRequest};
 
   constructor(private mongo: MongoProductsService, private date: DatePipe) {
   }
 
+  /**
+   * Gets all the records on the database as the page is loading
+   */
   ngOnInit() {
     this.mongo.getAll().subscribe(res => {
       this.results = res;
@@ -56,9 +59,15 @@ export class SearchComponent implements OnInit {
     });
   }
 
+  /**
+   * Search method called from the search button upon clicking
+   */
   search() {
+    // Reset the results found
     this.results = [];
+    // User the convert method to convert the form into a proper request
     let request = this.convert();
+    // Send a POST request to the spring boot back end
     this.mongo.query(request).subscribe(res => {
       this.results = res;
     }, err => {
@@ -66,8 +75,13 @@ export class SearchComponent implements OnInit {
     });
   }
 
+  /**
+   * Resets all the fields in the search form
+   */
   reset() {
-    this.productRequest = { ...this.defaultRequest };
+    this.productRequest = {...this.defaultRequest};
+
+    // Below 4 are required to return to a single value
     this.productRequest.cost.isMulti = false;
     this.productRequest.price.isMulti = false;
     this.productRequest.lastSold.isMulti = false;
@@ -86,6 +100,7 @@ export class SearchComponent implements OnInit {
       this.productRequest.lastSold.max = null;
     }
   }
+
   changeMultiShelfLife() {
     this.productRequest.shelfLife.isMulti = !this.productRequest.shelfLife.isMulti;
     if (this.productRequest.shelfLife.isMulti) {
@@ -95,6 +110,7 @@ export class SearchComponent implements OnInit {
       this.productRequest.shelfLife.max = null;
     }
   }
+
   changeMultiPrice() {
     this.productRequest.price.isMulti = !this.productRequest.price.isMulti;
     if (this.productRequest.price.isMulti) {
@@ -104,6 +120,7 @@ export class SearchComponent implements OnInit {
       this.productRequest.price.max = null;
     }
   }
+
   changeMultiCost() {
     this.productRequest.cost.isMulti = !this.productRequest.cost.isMulti;
     if (this.productRequest.cost.isMulti) {
@@ -118,26 +135,34 @@ export class SearchComponent implements OnInit {
    * Convert request into proper mongodb sql
    */
   convert() {
-    let tmpRequest = { ...this.productRequest};
+    // Creates a copy of the current form
+    let tmpRequest = {...this.productRequest};
+    // Iterates over the request's fields to get their values
     Object.keys(tmpRequest).forEach(key => {
+      // Get current value
       let value = tmpRequest[key];
+      // Check if value is null, remove from request if it is
       if (value === null) {
         delete tmpRequest[key];
+      // Checks if the field is a min/max field
       } else if (value.hasOwnProperty('isMulti')) {
+        // Removes from request if neither a value or a min/max value has been added
         if (!value.value && !value.max && !value.min) {
           delete tmpRequest[key];
+        // Checks if it is lastSold, due to dates needing to be sent as a certain format to the back end
         } else if (key === 'lastSold') {
+          // Converts last sold to the proper date format
           let lastSold = {
-              'isMulti': tmpRequest[key].isMulti,
-              'value': tmpRequest[key].value !== null ? this.date.transform(tmpRequest[key].value, 'yyyy-MM-dd HH:mm:ss.SSS') : null,
-              'min': tmpRequest[key].min !== null ? this.date.transform(tmpRequest[key].min, 'yyyy-MM-dd HH:mm:ss.SSS') : null,
-              'max': tmpRequest[key].max !== null ? this.date.transform(tmpRequest[key].max, 'yyyy-MM-dd HH:mm:ss.SSS') : null
+            'isMulti': tmpRequest[key].isMulti,
+            'value': tmpRequest[key].value !== null ? this.date.transform(tmpRequest[key].value, 'yyyy-MM-dd HH:mm:ss.SSS') : null,
+            'min': tmpRequest[key].min !== null ? this.date.transform(tmpRequest[key].min, 'yyyy-MM-dd HH:mm:ss.SSS') : null,
+            'max': tmpRequest[key].max !== null ? this.date.transform(tmpRequest[key].max, 'yyyy-MM-dd HH:mm:ss.SSS') : null
           };
           tmpRequest[key] = lastSold;
         }
       }
     });
-    console.log(this.productRequest, tmpRequest);
+    // Returns the updated request to be sent as the body to the back end
     return tmpRequest;
   }
 }
